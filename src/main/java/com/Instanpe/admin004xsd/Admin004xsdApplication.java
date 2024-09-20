cf1188b909efbba9341b1de21dc478fcc74ba16d
VasanthiReddy <vrvasreddy@gmail.com>
2024-09-20 11:15:25 +0530

    XSD-POJO

---
+++ src/main/java/com/Instanpe/admin004xsd/Admin004xsdApplication.java
@@ -0,0 +1,13 @@
+package com.Instanpe.admin004xsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Admin004xsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Admin004xsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Instanpe/admin004xsd/Admin004xsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Instanpe.admin004xsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Admin004xsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
